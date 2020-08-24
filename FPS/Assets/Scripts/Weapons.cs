80f814b39896e08e0fe1a7e87a920fd74e42f0b0
tikokito123 <30938070+tikokito123@users.noreply.github.com>
2020-08-24 18:19:32 +0300

    muzzle flash

---
+++ FPS/Assets/Scripts/Weapons.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapons : MonoBehaviour
+{
+    [SerializeField] Camera fpCamera;
+    [SerializeField] float range = 100f;
+    [SerializeField] float damage = 10f;
+    [SerializeField] ParticleSystem muzzleFlash;
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            shoot();
+        }     
+    }
+
+    private void shoot()
+    {
+        PlayMuzzleFlash();
+        ProcessRaycast();
+    }
+
+    private void PlayMuzzleFlash()
+    {
+        muzzleFlash.Play();
+    }
+
+    private void ProcessRaycast()
+    {
+        RaycastHit hit;
+        var hitEnemies = Physics.Raycast(fpCamera.transform.position, fpCamera.transform.forward, out hit, range);
+        if (hitEnemies)
+        {
+            print("hit " + hit.transform.name);
+            // TODO: add particle effect
+            EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
+            if (target == null) { return; }
+            target.TakeDamage(damage);
+        }
+        else
+        {
+            return;
+        }
+    }
+}
