618cc942c5d9c37ae61bea9fb9abdc35cb7deeec
tikokito123 <30938070+tikokito123@users.noreply.github.com>
2020-08-25 22:05:55 +0300

    Player Input to switch weapons!

--- FPS/Assets/Scripts/WeaponSwicher.cs
+++ FPS/Assets/Scripts/WeaponSwicher.cs
@@ -2,22 +2,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.PlayerLoop;
 
 public class WeaponSwicher : MonoBehaviour
 {
-    [SerializeField] int currentweapon = 0;
+    [SerializeField] int currentWeapon = 0;
     // Start is called before the first frame update
     void Start()
     {
         SetWeaponActive();
     }
-
+    void Update()
+    {
+        int previousWeapon = currentWeapon;
+        ProcessKey();
+        ProcessControlWheel();
+        if (previousWeapon != currentWeapon)
+        {
+            SetWeaponActive();
+        }
+    }
+    private void ProcessControlWheel()
+    {
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            if (currentWeapon >= transform.childCount - 1)
+            {
+                currentWeapon = 0;
+            }
+            else
+            {
+                currentWeapon++;
+            }
+        }
+    }
     private void SetWeaponActive()
     {
         int weaponIndex = 0;
         foreach (Transform weapon in transform)
         {
-            if (weaponIndex == currentweapon)
+            if (weaponIndex == currentWeapon)
             {
                 weapon.gameObject.SetActive(true);
             }
@@ -28,8 +53,27 @@ public class WeaponSwicher : MonoBehaviour
             weaponIndex++;
         }
     }
-    void Update()
+    private void ProcessKey()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            currentWeapon = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            currentWeapon = 1;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            currentWeapon = 2;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            currentWeapon = 3;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            currentWeapon = 4;
+        }
     }
 }
