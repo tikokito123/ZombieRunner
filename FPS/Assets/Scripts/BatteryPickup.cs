eb7e6f6c6418be31243779a9b4144922c8a016c6
tikokito123 <30938070+tikokito123@users.noreply.github.com>
2020-08-28 15:18:57 +0300

    Added a flash light and battery

---
+++ FPS/Assets/Scripts/BatteryPickup.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatteryPickup : MonoBehaviour
+{
+    FlashLight flash;
+    [SerializeField] float restoreAngel = 5f;
+    [SerializeField] float restoreIntencity = 3.5f;
+    void Start()
+    {
+        flash = FindObjectOfType<FlashLight>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            Restore();
+        }
+        else
+        {
+            return;
+        }
+    }
+    private void Restore()
+    {
+        flash.RestoreAngel(restoreAngel);
+        flash.RestoreLightIntensity(restoreIntencity);
+    }
+}
---
+++ FPS/Assets/Scripts/FlashLight.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashLight : MonoBehaviour
+{
+    [SerializeField] float lightDecay = 0.1f;
+    [SerializeField] float angelDecay = 1f;
+    [SerializeField] float minimunAngel = 40f;
+    Light myLight;
+    void Start()
+    {
+        myLight = GetComponent<Light>();
+    }
+    void Update()
+    {
+        DecreaseLightAngel();
+        DecreaseLightDecay();
+    }
+
+    public void RestoreAngel(float restoreAngel)
+    {
+        myLight.spotAngle = restoreAngel;
+    }
+    public void RestoreLightIntensity(float restoreIntensity)
+    {
+        myLight.intensity += restoreIntensity;
+    }
+    private void DecreaseLightAngel()
+    {
+        if (myLight.spotAngle > minimunAngel)
+        {
+            myLight.spotAngle -= angelDecay * Time.deltaTime;
+        }
+        else
+        {
+            return; 
+        }
+    }
+    private void DecreaseLightDecay()
+    {
+        myLight.intensity -= lightDecay * Time.deltaTime;
+    }
+}
